2a764b9c35383174be38b5f8c377ed4d69e95367
Bastian Somon <bastian.somon@gmail.com>
2022-09-28 14:38:39 +0200

    Add .dockerignore

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-docker volume create nginx-server-volume
+docker stop nginx-server
+
+docker volume create --name nginx-server-volume
 
 docker build . -t nginx-server:nginx-server
 
-docker run  \
+# If a docker container named 'nginx-server' already exists, just start it
+docker start nginx-server || docker run  \
    --name nginx-server \
    -d \
    -p 80:80 \
    -p 2222:22 \
-   -v nginx-sever-volume:/volume \
+   -v nginx-server-volume:/volume \
    nginx-server:nginx-server
